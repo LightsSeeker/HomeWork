e8b06278ad86d417e24251be3921d0adfe3f3dfc
asazonov <alexeysazonow@gmail.com>
2023-06-15 02:27:45 +0300

    add lession2/task1

--- lession2/task1/Program.cs
+++ lession2/task1/Program.cs
@@ -2,3 +2,27 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+bool Validate3sign(int number)
+{
+    if(number < 100 || number >= 1000)
+    {
+        Console.WriteLine("Вы ввели не 3-хзначное число");
+        return false;
+    }
+    return true;
+}
+
+int digit = ReadInt("Введите 3-хзначное число > ");
+if (Validate3sign(digit))
+{
+    int lastDigit = digit /10 % 10;
+    Console.WriteLine($"Вторая цифра числа {digit} равна {lastDigit}");
+}
