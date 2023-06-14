77458af30efe9a3b8250bcef6bca37b15dbc0455
asazonov <alexeysazonow@gmail.com>
2023-06-15 01:45:45 +0300

    1 and 2 task

--- lession1/task2/Program.cs
+++ lession1/task2/Program.cs
@@ -7,3 +7,19 @@ Console.WriteLine("Введите первое число ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число ");
 int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = {a,b,c};                                // создаём массив из введённых чисел
+int massiveSize = 3; 
+int currentIndex = 0;
+int max = numbers[0];
+while (currentIndex < massiveSize)                      // проходим циклом по массиву, сравнивая числа с max, и меняем max, если число больше
+{
+if ( numbers[currentIndex] > max ) 
+{
+    max = numbers[currentIndex];
+}
+currentIndex++;
+}
+Console.WriteLine("Максимальное число " + max);
