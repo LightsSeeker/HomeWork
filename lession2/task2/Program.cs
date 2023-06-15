e36b682082a07cadf84a3a82793ee059ef701d43
asazonov <alexeysazonow@gmail.com>
2023-06-15 19:05:32 +0300

    add task2

--- lession2/task2/Program.cs
+++ lession2/task2/Program.cs
@@ -3,3 +3,34 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int CheckThird(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if(number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+
+int number = ReadInt("Введите число > ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(CheckThird(number));
+}
