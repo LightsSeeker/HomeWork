a2c59c84f2315c5d740a5eb98ad09b4d7fc13dc9
lightsseeker <mydesstiny@yandex.ru>
2023-06-23 08:27:58 +0300

    add tasks

--- lession3/task3/Program.cs
+++ lession3/task3/Program.cs
@@ -2,3 +2,16 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string message)
+{
+    Console.Write($"{message}");
+    return int.Parse(Console.ReadLine());
+}
+
+int number = Prompt("Введите положительное число:");
+
+for (int N = 1; N <= number; N++)
+{
+    Console.WriteLine(Math.Pow(N, 3));
+}
