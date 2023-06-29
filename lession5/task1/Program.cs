f1f9339415cc90909a8edf6b3904673ebd251516
lightsseeker <mydesstiny@yandex.ru>
2023-06-29 09:10:05 +0300

    add tasks

--- lession5/task1/Program.cs
+++ lession5/task1/Program.cs
@@ -1,63 +1,33 @@
 ﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
-int count = 0;
-
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
-
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
-
-void FillArrayRandomNumbers(int[] numbers)
+void ArrayRandomNumbers(int[] numbers)
 {
     for(int i = 0; i < numbers.Length; i++)
     {
         numbers[i] = new Random().Next(100,1000);
     }
 }
-void PrintArray(int[] numbers)
+
+void PrintArray(int[] array)
 {
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        Console.Write($"{array[i]} ");
     }
-    Console.Write("]");
-    Console.WriteLine();
+    Console.WriteLine("]");
 }
 
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+ArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
 
-// Console.Write(«Введите количество элементов массива: «);
-// int a = Convert.ToInt32(Console.ReadLine());
-// int[] randomArray = new int[a];
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
 
-// void mas(int a)
-// {
-// for (int i = 0; i < a; i++)
-// {
-// randomArray[i] = new Random().Next(99,999);
-// Console.Write(randomArray[i] + » «);
-// }
-
-// }
-
-// int kol(int[] randomArray)
-// {
-// int kol = 0;
-// for (int i = 0; i < randomArray.Length; i++)
-// {
-// if (randomArray[i] % 2 == 0)
-// kol = kol + 1;
-// }
-// return kol;
-// }
-
-// mas(a);
-// Console.Write($»\nКоличество чётных чисел в массиве: {kol(randomArray)}»);
+Console.WriteLine($"В массиве {numbers.Length} чисел, {count} из них чётные");
